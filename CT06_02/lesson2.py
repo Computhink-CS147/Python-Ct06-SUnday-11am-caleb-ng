d75b537a646bd476cc161b9e42162098e4748802
Computhink-CS147 <cs147@computhink.com.sg>
2026-01-18 04:25:33 +0000

    end of lesson 2

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,41 @@
 print("Hello from lesson 2")
-
+Print("Maroon")
+Print("Copper")
+Print("Gold")
+Print("Green")
+Print("Purple")
+Print("black")
+Print("White")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
-
+# on start
+# repeat 10x :
+# say Hey!
+# move 10 steps
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+# on start 
+#set counter to 0
+# repeat until counter=50
+# move 10 steps
+# turn 15 degrees
+# change "counter" by 10
+# on start
+#ask what is ur age
+# if answer is lower than 18
+#access denied
+# else
+#welcome
+# recycling robot:
+#forever
+# accept 1 item from the customer at 1 time
+# use the sensor to check the material if it is glass then
+#accept
+# else if its plastic 
+#accept
+#else if it is paper
+#accept
+# else reject the item
+#ask what is the password "idk"
+#if the answer is not idk then access denied
+#else access allowed
