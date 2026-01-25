5354229f6c625ac877fde0555d6746132417926b
Computhink-CS147 <cs147@computhink.com.sg>
2026-01-25 04:24:24 +0000

    end of lesson 3

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,30 @@
 print("Hello from lesson 3")
+print("my sentence")
+print("age")
+print("x")
+x=5
+x
+age=10
+print(age)
+varia1=25
+varia2=42
+print(varia1+varia2)
+print(varia1-varia2)
+print(varia1*varia2)
+print(varia1/varia2)
+pi=3.14159265358979323846264338327950288418
+pai=3.141592653589793238462643383279502884167
+print(pi+pai)
+print(pi)
+print(pai)
+# print(50-10)*3
+print(100/4)
+print((3+4)*5-(10/2))
+print(15*2)
+print(58)
+print(10/100* 96+18*3+2*6)
+total= 96+(18*3)+(2*6)
+service_charge = 10/100* total
+total=total + service_charge
+total=total -25 # use discount voucher
+print(total)
