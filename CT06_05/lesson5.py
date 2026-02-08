474d345150f5f15941753833b1c297ed2ba3aa14
Computhink-CS147 <cs147@computhink.com.sg>
2026-02-08 04:20:45 +0000

    end of lesson 5

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,22 @@
-print("Hello from lesson 5")
+# print("Hello from lesson 5")
+
+# yourage= input("how old is he this year")
+# yourname= input("what is that person's name")
+# message= imput("what is your secret message")
+
+# print("happy", yourage "birthday", Yourname,".",Message)
+# for count in range(100):
+#     print("I like chicken rice")
+#     print("67")
+#     print ("i like cake give me more")
+#     print("gimme more")
+# myword="lady gaga is awesome"
+# for letter in myword:
+#     print("give me A", letter)
+# print("David is", myword)
+# # Range(0-59)
+range (2-24)
+print (range)
+n1 =imput("give me a number")
+n2 = int(N1) # Convert
+    
