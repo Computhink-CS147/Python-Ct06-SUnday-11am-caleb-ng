4bb470cc89b3212871b06b273c1c6fdc05809514
Computhink-CS147 <cs147@computhink.com.sg>
2026-03-29 04:28:40 +0000

    end of lesson ten

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -8,6 +8,7 @@
 # all_even_odd = (num1%2==0) and (num2%2==0) and (num3%2==0)
 # print("all numbers are even/odd:", all_even_odd)
 
+import random
 numapples = random.randint(1, 10)
 numoranges = random.randint(1,10)
 if numapples>5:
@@ -18,6 +19,10 @@ else:
         costoranges= numoranges * 0.90
     else:
         costoranges = numoranges *0.90 * 90
-totalcost= costapples + costoranges
+totalcost = costapples + costoranges
 print (totalcost)
 print("end")
+s=input("how many apples to buy")
+if count>5:
+    cost = count *10
+else: print('cost is',) ''
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,22 @@
 print("Hello from lesson 10")
+print("")
+
+# import random
+# anynum = random.randint(1,15)
+# guess= int(input("guess the number iam thinking"))import random
+# 
+# age = int(input("what is your age?"))
+# if age >12 :
+#     print("you are a kid")
+# elif age >20 and age <12:
+#     print("you are a teenager")
+# else: age <20 
+# print("you are an adult")
+# 
+# score = int(input("what is your score?"))
+# if score in range (90,101):
+#     print("you got an Al 1!")
+# elif score in range (85,90):
+#     print("you got a Al 2!")
+# else:
+#     print("you got a Al3 or below.")
