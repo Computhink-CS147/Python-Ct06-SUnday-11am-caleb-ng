6f622b20863b471177e2c23e3105d7e3e84a8b55
Computhink-CS147 <cs147@computhink.com.sg>
2026-04-05 04:29:32 +0000

    end of 11

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,29 @@
-print("Hello from lesson 11")
+# print("Hello from lesson 11")
+# number = int(input("Enter a number: "))
+# divisibleby3 = number % 3 == 0
+# divisibleby7 = number % 7 == 0
+# if divisibleby3 and divisibleby7:
+#     print("The number is a multiple of both 3 and 7.")
+# else:
+#     print("The number is not a multiple of both 3 and 7.")
+Rider1=25
+Rider2=6
+age=17
+# if age >= 18:
+#     print("You are old enough to ride.")
+# else:
+#     print("You are not old enough to ride.")
+# int(input("what is your age?"))
+# ticket=20
+# if age >= 18:
+#     print("you have to pay the full price of $20")
+# elif age < 12:
+#     print("nahh id win you can pay 10$")
+# elif age < 60:
+#     print("you can play 15$")
+# color = input("what color do you want?")
+# isgreen=color == "green"
+# if not isgreen:
+#     print("try again")
+# else:
+#     print("green is the best color")
