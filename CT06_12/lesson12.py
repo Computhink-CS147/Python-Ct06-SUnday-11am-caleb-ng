0dd7f5460dba97c389af5767bb042274194de323
Computhink-CS147 <cs147@computhink.com.sg>
2026-04-12 04:27:56 +0000

    done12

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,37 @@
-print("Hello from lesson 12")
+# print("Hello from lesson 12")
+# number=int(input("Enter a number: "))
+# divisibleby3=number%3==0
+# divisibleby5=number%5==0
+# if divisibleby3 and divisibleby5:
+#     print("The number is a multiple of both 3 and 5.")
+# else:
+#     print("The number is not a multiple of both 3 and 5.")
+# visitors=int(input("How many visitors are there? "))
+# if visitors>50:
+#     print("wait")
+# elif visitors<49:
+#     print("you can enter")
+# counter=1
+# while True: 
+#     print(counter)
+#     if counter == 30:
+#         break
+#     counter=counter+1
+# final_order=""
+# reply = input("what do you want to order? ")
+# while not (reply == "end"):
+#     final_order= final_order +", " + reply
+#     reply = input("what do you want to order? ")
+# print("Your final order is: " + final_order)
+# while num != 0:
+#     print(num)
+#     num-=1
+# else:
+#     print("Happy new year!")
+while count > 0:
+    print(count)
+    count=count-1
+    if count ==5"
+        break
+else:
+    print("Happy new year!")
